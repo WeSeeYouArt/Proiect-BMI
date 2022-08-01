5780c495547b828ddaa95dd459f215a576a97d73
WeSeeYouArt <alexandrupotirlau@gmail.com>
2022-08-01 18:05:23 +0300

    updates

--- Proiect BMI.js
+++ Proiect BMI.js
@@ -1,4 +1,5 @@
 document.querySelector('.btn').addEventListener('click', function() {
+
     const name = document.querySelector('.numele').textContent;
     // const year = document.querySelector('.anulNasterii').value;
     const height = document.querySelector('.inaltimea').value;
@@ -6,5 +7,27 @@ document.querySelector('.btn').addEventListener('click', function() {
 
     bmi = weight / (height * height);
 
-document.querySelector('.rezultatBMI').textContent = [`BMI-ul este: ${bmi}`];
+    if(bmi < 18.5){
+        category = "Underweight 😒";
+    }
+
+  //If BMI is >=18.5 and <=24.9
+    else if( bmi >= 18.5 && bmi <= 24.9 ){
+        category = "Normal Weight 😍";
+    }
+
+  //If BMI is >= 25 and <= 29.9 
+    else if( bmi >= 25 && bmi <= 29.9 ){
+        category = "Overweight 😮";
+    }
+
+  //If BMI is <= 30
+    else{
+        category = "Obese 😱";
+    }
+  //All of the above text is stored in "category".
+
+//Now you have to make arrangements to display the information in the webpage with the help of "textContent"
+document.getElementById("category").textContent = [`BMI-ul este ${bmi} / ${category}`];
+// document.querySelector('.rezultatBMI').textContent = [`BMI-ul este: ${bmi}`]
 })

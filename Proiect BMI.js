4f249d984a3e1646e10a696e3bc4bd0821a5c173
WeSeeYouArt <alexandrupotirlau@gmail.com>
2022-08-03 11:56:53 +0300

    updates

--- Proiect BMI.js
+++ Proiect BMI.js
@@ -1,33 +1,24 @@
 document.querySelector('.btn').addEventListener('click', function() {
 
-    const name = document.querySelector('.numele').textContent;
-    // const year = document.querySelector('.anulNasterii').value;
-    const height = document.querySelector('.inaltimea').value;
-    const weight = document.querySelector('.greutatea').value;
+    let name1 = document.querySelector('.name1').textContent;
+    let name2 = document.querySelector('.name2').textContent;
+    let height1 = document.querySelector('.height1').value;
+    let height2 = document.qquerySelector('.height2').value;
+    let weight1 = document.querySelector('.weight1').value;
+    let weight2 = document.querySelector('.weight2').value;
 
-    bmi = weight / (height * height);
+    let bmi1;
+    let bmi2;
 
-    if(bmi < 18.5){
-        category = "Underweight 😒";
+    bmi1 = weight1 / (height1 ** 2);
+    bmi2 = weight2 / (height2 ** 2);
+
+document.querySelector('.result1').textContent = (`BMI lui ${name1} este ${bmi1}`);
+document.querySelector('.result2').textContent = (`BMI lui ${name2} este ${bmi2}`);
+
+if (bmi1 > bmi2) {
+        document.querySelector('.compare-result').textContent = (`${name1} are BMI-ul mai mare.`);
+    } else {
+        document.querySelector('.compare-result').textContent = (`${name2} are BMI-ul mai mare.`);
     }
-
-  //If BMI is >=18.5 and <=24.9
-    else if( bmi >= 18.5 && bmi <= 24.9 ){
-        category = "Normal Weight 😍";
-    }
-
-  //If BMI is >= 25 and <= 29.9 
-    else if( bmi >= 25 && bmi <= 29.9 ){
-        category = "Overweight 😮";
-    }
-
-  //If BMI is <= 30
-    else{
-        category = "Obese 😱";
-    }
-  //All of the above text is stored in "category".
-
-//Now you have to make arrangements to display the information in the webpage with the help of "textContent"
-document.getElementById("category").textContent = [`BMI-ul este ${bmi} / ${category}`];
-// document.querySelector('.rezultatBMI').textContent = [`BMI-ul este: ${bmi}`]
 })
